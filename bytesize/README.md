1f710bbb73394be294f5f9e7e5be55be6a89de05
shenwei356 <shenwei356@gmail.com>
2014-02-20 11:51:44 +0800

    add func Parse to package bytesize

--- bytesize/README.md
+++ bytesize/README.md
@@ -2,7 +2,8 @@ bytesize
 ========
 
 Package for providing a way to show readable values of byte sizes by reediting
-the code from http://golang.org/doc/effective_go.html.
+the code from http://golang.org/doc/effective_go.html. It could also parseng
+byte size text to ByteSize object.
 
 Install
 -------
@@ -13,13 +14,24 @@ This package is "go-gettable", just:
 Example
 -------
     
-    fmt.Printf("1024 bytes = %v\n", ByteSize(float64(1024)))
-    fmt.Printf("13146111 bytes = %v\n", ByteSize(float64(13146111)))
+	fmt.Printf("1024 bytes\t%v\n", bytesize.ByteSize(float64(1024)))
+	fmt.Printf("13146111 bytes\t%v\n", bytesize.ByteSize(float64(13146111)))
+
+    // parsing
+	size, err := bytesize.Parse([]byte("1.5 KB"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%.0f bytes\n", size)
+
 
 Result:
 
-    1024 bytes =   1.00 KB
-    13146111 bytes =  12.54MB
+    1024 bytes         1.00 KB
+    13146111 bytes    12.54 MB
+    1536 bytes
+
+
 
 Copyright (c) 2013, Wei Shen (shenwei356@gmail.com)
 
--- bytesize/bytesize.go
+++ bytesize/bytesize.go
@@ -1,20 +1,25 @@
-// Copyright 2013 Wei Shen (shenwei356@gmail.com). All rights reserved.
+// Copyright 2014 Wei Shen (shenwei356@gmail.com). All rights reserved.
 // Use of this source code is governed by a MIT-license
 // that can be found in the LICENSE file.
 
-// Package bytesize provides a way to show readable values of byte sizes
+// Package bytesize provides a way to show readable values of byte size
 // by reediting the code from http://golang.org/doc/effective_go.html.
+// It could also parseng byte size text to ByteSize object.
 package bytesize
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type ByteSize float64
 
 const (
-	_           = iota
-	KB ByteSize = 1 << (10 * iota)
+	B ByteSize = 1 << (10 * iota)
+	KB
 	MB
 	GB
 	TB
@@ -24,6 +29,7 @@ const (
 	YB
 )
 
+// Print readable values of byte size
 func (b ByteSize) String() string {
 	switch {
 	case b >= YB:
@@ -44,4 +50,56 @@ func (b ByteSize) String() string {
 		return fmt.Sprintf("%7.2f KB", b/KB)
 	}
 	return fmt.Sprintf("%7.2f  B", b)
+}
+
+// Regexp object for ByteSize Text
+var BytesizeRegexp = regexp.MustCompile(`(?i)^\s*([\-\d\.]+)\s*(B|KB|MB|GB|TB|PB|EB|ZB|YB)\s*$`)
+
+// Error information for Illegal byte size text
+var ErrText = "Illegal bytesize text"
+
+// Parse ByteSize Text to ByteSize object
+//
+// Example
+//
+//     size, err := bytesize.Parse([]byte("1.5 KB"))
+//     if err != nil {
+//         fmt.Println(err)
+//     }
+//     fmt.Printf("%.0f bytes\n", size)
+//
+func Parse(sizeText []byte) (ByteSize, error) {
+	if !BytesizeRegexp.Match(sizeText) {
+		return 0, errors.New(ErrText)
+	}
+
+	// parse value and unit
+	subs := BytesizeRegexp.FindSubmatch(sizeText)
+
+	// no need to check ParseFloat error. BytesizeRegexp could ensure this
+	size, _ := strconv.ParseFloat(string(subs[1]), 64)
+	unit := strings.ToUpper(string(subs[2]))
+
+	switch unit {
+	case "B":
+		size = size * float64(B)
+	case "KB":
+		size = size * float64(KB)
+	case "MB":
+		size = size * float64(MB)
+	case "GB":
+		size = size * float64(GB)
+	case "TB":
+		size = size * float64(TB)
+	case "PB":
+		size = size * float64(PB)
+	case "EB":
+		size = size * float64(EB)
+	case "ZB":
+		size = size * float64(ZB)
+	case "YB":
+		size = size * float64(YB)
+	}
+
+	return ByteSize(size), nil
 }
