66be9ed791a0e9662e73727d67326cc34c6f8453
shenwei356 <shenwei356@gmail.com>
2014-05-23 14:44:33 +0800

    update doc of ParallelMap

--- ParallelMap/ParallelMap.go
+++ ParallelMap/ParallelMap.go
@@ -9,34 +9,52 @@
 //
 // Example:
 //
-//    N := runtime.NumCPU() * 30
-//    runtime.GOMAXPROCS(N)
+//    import (
+//        "fmt"
+//        "runtime"
+//        "sort"
+//        "sync"
 //
-//    m := NewParallelMap(func(v1 ValueType, v2 ValueType) ValueType {
-//        return v1.(int) + v2.(int)
-//    }, N)
+//        pmap "github.com/shenwei356/util/ParallelMap"
+//    )
 //
-//    var length int = 1 << 10
+//    func main() {
+//        // number of goroutines that will operate on ParallelMap
+//        N := runtime.NumCPU() * 30
+//        runtime.GOMAXPROCS(N)
 //
-//    var wg sync.WaitGroup
-//    for i := 1; i <= N; i++ {
-//        wg.Add(1)
+//        // constructor
+//        m := pmap.NewParallelMap(func(v1 pmap.ValueType, v2 pmap.ValueType) pmap.ValueType {
+//            return v1.(int) + v2.(int)
+//        }, N)
 //
-//        go func() {
-//            defer func() {
-//                wg.Done()
-//                m.UnboundAGoroutine()
+//        // number of elements in map
+//        var n int = 1 << 9
+//
+//        var wg sync.WaitGroup
+//        for i := 1; i <= N; i++ {
+//            wg.Add(1)
+//
+//            go func() {
+//                defer func() {
+//                    wg.Done()
+//                    m.UnboundAGoroutine()
+//                }()
+//
+//                for j := 0; j < n; j++ {
+//                    // update data in map
+//                    m.Update(j, 1)
+//                }
 //            }()
+//        }
 //
-//            for j := 0; j < length; j++ {
-//                m.Update(j, 1)
-//            }
-//        }()
-//    }
+//        // wait for all operations to complement
+//        wg.Wait()
+//        m.Wait()
 //
-//    // wait for all operations to complement
-//    wg.Wait()
-//    m.Wait()
+//        // do something else
+//        length := len(m.Map)
+//        fmt.Printf("%d elements in map\n", length)
 //
 package ParallelMap
 
