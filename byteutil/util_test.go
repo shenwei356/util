1b05f94371e1911b8b43f62bb0fb11c60e7d38e3
shenwei356 <shenwei356@gmail.com>
2016-03-22 19:45:12 +0800

    add String2ByteSlice tool to stringutil

--- byteutil/util_test.go
+++ byteutil/util_test.go
@@ -4,12 +4,11 @@ import "testing"
 
 func TestSubSlice(t *testing.T) {
 	s := []byte("0123456789")
-
-	if string(SubSlice(s, 0, 0)) == "0123456789" &&
+	if true &&
+		string(SubSlice(s, 0, 0)) == "0123456789" &&
 		string(SubSlice(s, 0, 1)) == "0" &&
 		string(SubSlice(s, 1, 2)) == "1" &&
 		string(SubSlice(s, -2, -1)) == "8" &&
-		string(SubSlice(s, -2, len(s))) == "89" &&
 		string(SubSlice(s, len(s)-1, len(s))) == "9" &&
 		string(SubSlice(s, -1, 0)) == "9" && // different from python
 		string(SubSlice(s, 7, -1)) == "78" &&
