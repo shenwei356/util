055178d5f1da967ca7b4a83d1ebbf456e0fcfa70
shenwei356 <shenwei356@gmail.com>
2016-05-12 20:27:49 +0800

    add byteutil/ByteCoder

--- math/float.go
+++ math/float.go
@@ -7,3 +7,8 @@ func Round(f float64, n int) float64 {
 	pow10N := math.Pow10(n)
 	return math.Trunc((f+0.5/pow10N)*pow10N) / pow10N
 }
+
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
