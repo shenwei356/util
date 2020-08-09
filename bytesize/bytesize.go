4d78997fbf137369334529d68c1b4ae66ea82432
Wei Shen <shenwei356@gmail.com>
2020-08-09 19:30:39 +0800

    bytesize: removing leading blanks

--- bytesize/bytesize.go
+++ bytesize/bytesize.go
@@ -35,23 +35,23 @@ const (
 func (b ByteSize) String() string {
 	switch {
 	case b >= YB:
-		return fmt.Sprintf("%7.2f YB", b/YB)
+		return fmt.Sprintf("%.2f YB", b/YB)
 	case b >= ZB:
-		return fmt.Sprintf("%7.2f ZB", b/ZB)
+		return fmt.Sprintf("%.2f ZB", b/ZB)
 	case b >= EB:
-		return fmt.Sprintf("%7.2f EB", b/EB)
+		return fmt.Sprintf("%.2f EB", b/EB)
 	case b >= PB:
-		return fmt.Sprintf("%7.2f PB", b/PB)
+		return fmt.Sprintf("%.2f PB", b/PB)
 	case b >= TB:
-		return fmt.Sprintf("%7.2f TB", b/TB)
+		return fmt.Sprintf("%.2f TB", b/TB)
 	case b >= GB:
-		return fmt.Sprintf("%7.2f GB", b/GB)
+		return fmt.Sprintf("%.2f GB", b/GB)
 	case b >= MB:
-		return fmt.Sprintf("%7.2f MB", b/MB)
+		return fmt.Sprintf("%.2f MB", b/MB)
 	case b >= KB:
-		return fmt.Sprintf("%7.2f KB", b/KB)
+		return fmt.Sprintf("%.2f KB", b/KB)
 	}
-	return fmt.Sprintf("%7.2f  B", b)
+	return fmt.Sprintf("%.2f B", b)
 }
 
 // Regexp object for ByteSize Text. The REGEXP is:
